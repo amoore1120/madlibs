af360db2852fffca60243e84c8b5d6e42442e777
amoore1120 <amoore1120ka@gmail.com>
2023-06-05 12:11:08 -0400

    finalUntilIFindSolutionToTheRequired

--- app.js
+++ app.js
@@ -19,13 +19,6 @@ function createParagraph() {
 
     document.getElementById('answer').innerHTML = paragraph; 
 }
-
-
-/*
-var paragraph = <p>'Do not ' + '<span class="highlight">' + wordOne + '</span>' + ' to me before I have had my coffee! Before I get my first sip, I am ' + '<span class="highlight">' + wordTwo + '</span>' + '. If I do not get to practice my morning routine, then the day is sure to be ' + '<span class="highlight">' + wordThree + '</span>' + '. First, I get my ' + '<span class="highlight">' + wordFour +'</span>' +  " " +'<span class="highlight">' +  wordFive + '</span>' + ' mug. Then, I brew some ' + '<span class="highlight">' + wordSix + '</span>' + ' coffee. Next, I mix in ' + '<span class="highlight">' + wordSeven + '</span>' + '. I enjoy drinking this coffee while reading ' + '<span class="highlight">' + wordEight + '</span>' + ' . Once the ritual is complete, I feel ' + '<span class="highlight">' + wordNine + '</span>' + '. When I miss my java break, I turn into a ' + '<span class="highlight">' + wordTen + '</span>' + ' ' + '<span class="highlight">' + wordEleven + '</span>' + '.'; </p>
-*/
-
-
 /*  input values
     _1_verb_
     _2_type_of_feeling__
@@ -47,5 +40,88 @@ var paragraph = <p>'Do not ' + '<span class="highlight">' + wordOne + '</span>' 
 
 
 
+/*      POSSIBLE SOLUTION TO THE REQUIRED ATTRIBUTE  ON HTML INPUT FIELD NOT WORKING
 
+*       MORE INFO IN READ ME
 
+function createParagraph() {
+    var verbInput = document.getElementById('verb');
+    var feelingInput = document.getElementById('feeling');
+    var adj1Input = document.getElementById('adj1');
+    var adj2Input = document.getElementById('adj2');
+    var nounInput = document.getElementById('noun');
+    var flavorInput = document.getElementById('flavor');
+    var foodInput = document.getElementById('food');
+    var readInput = document.getElementById('read');
+    var adj3Input = document.getElementById('adj3');
+    var adj4Input = document.getElementById('adj4');
+    var monsterInput = document.getElementById('monster');
+
+    if (!verbInput.value) {
+        verbInput.setCustomValidity('Please enter a verb.');
+        verbInput.reportValidity();
+        return;
+    }
+
+    if (!feelingInput.value) {
+        feelingInput.setCustomValidity('Please enter a type of feeling.');
+        feelingInput.reportValidity();
+        return;
+    }
+
+    if (!adj1Input.value) {
+        adj1Input.setCustomValidity('Please enter an adjective.');
+        adj1Input.reportValidity();
+        return;
+    }
+
+    if (!adj2Input.value) {
+        adj2Input.setCustomValidity('Please enter an adjective.');
+        adj2Input.reportValidity();
+        return;
+    }
+
+    if (!nounInput.value) {
+        nounInput.setCustomValidity('Please enter a noun.');
+        nounInput.reportValidity();
+        return;
+    }
+
+    if (!flavorInput.value) {
+        flavorInput.setCustomValidity('Please enter a flavor.');
+        flavorInput.reportValidity();
+        return;
+    }
+
+    if (!foodInput.value) {
+        foodInput.setCustomValidity('Please enter a type of food.');
+        foodInput.reportValidity();
+        return;
+    }
+
+    if (!readInput.value) {
+        readInput.setCustomValidity('Please enter something you read.');
+        readInput.reportValidity();
+        return;
+    }
+
+    if (!adj3Input.value) {
+        adj3Input.setCustomValidity('Please enter an adjective.');
+        adj3Input.reportValidity();
+        return;
+    }
+
+    if (!adj4Input.value) {
+        adj4Input.setCustomValidity('Please enter an adjective.');
+        adj4Input.reportValidity();
+        return;
+    }
+
+    if (!monsterInput.value) {
+        monsterInput.setCustomValidity('Please enter a type of monster.');
+        monsterInput.reportValidity();
+        return;
+    }
+
+    var paragraph = '<p>Do not ' + '<span class="highlight">' + verbInput.value + '</span>' + ' to me before I have had my coffee! Before I get my first sip, I am ' + '<span class="highlight">' + feelingInput.value + '</span>' + '. If I do not get to practice my morning routine, then the day is sure to be ' + '<span class="highlight">' + adj1Input.value + '</span>' + '. First, I get my ' + '<span class="highlight">' + adj2Input.value + '</span>' + ' ' + '<span class="highlight">' + nounInput.value + '</span>' + ' mug. Then, I brew some ' + '<span class="highlight">' + flavorInput.value + '</span>' + ' coffee. Next, I mix in ' + '<span class="highlight">' + foodInput.value + '</span>' + '. I enjoy drinking this coffee while reading ' + '<span class="highlight">' + readInput.value + '</span>' + '. Once the ritual is complete, I feel ' + '<span class="highlight">' + adj3Input.value + '</span>' + '. When I miss my java break, I turn into a ' + '<span class
+*/
